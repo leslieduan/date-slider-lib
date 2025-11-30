16a75036f2c4f75c9258b97fbc9c55a357e2fd2e
leslie <leslie@leslies-MacBook-Pro.local>
2025-11-30 16:24:43 +1100

    chore: remove dead code

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
-export * from './useDidMountEffect';
 export * from './useDrag';
 export * from './useElementSize';
 export * from './useRAFDFn';
 export * from './useResizeObserver';
-export * from './useToggle';
--- src/hooks/useToggle.ts
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-
-export function useToggle(v: boolean) {
-  const [open, setOpen] = useState(v);
-  return { open, toggle: () => setOpen((prev) => !prev) };
-}
